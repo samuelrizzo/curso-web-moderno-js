4aadbde0e6a7fccc9f02b9628baae203a9ce05d2
rizzo <samuelrizzo5@gmail.com>
2023-02-25 13:43:07 -0300

    aula 3.9

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'Legal';
+
+console.log(qualquer);
+console.log(typeof qualquer)
+
+qualquer = 3.156
+
+console.log(qualquer);
+console.log(typeof qualquer)
+
+// Evitar nome genéricos e siglas
+
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso ... kkk
