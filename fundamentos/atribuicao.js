871b96c5a5485c94d7dce7d1d64810e14ef12dd5
rizzo <samuelrizzo5@gmail.com>
2023-03-05 08:29:50 -0300

    aula 71 finalizada

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,5 @@
+const a = 7
+let b = 3
+
+b += a
+console.log(b)
---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,19 @@
+const saudacao = 'Opa'
+
+function exec () {
+    const saudacao = 'Falaaa'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Samuel',
+    idade: '29',
+    peso: 117,
+    endereco: {
+        rua: 'Rua 10',
+        numero: 16
+    }
+}
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
